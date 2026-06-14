69a55d028296bd80425eab07de7aa7d7b83a5daf
Varun Dhyani <varundhyani29@gmail.com>
2026-06-14 22:18:37 +0530

    Implemented login controller and orute and made authentication middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const { v4: uuidv4 } = require("uuid");
 const pool = require("../config/connection.js");
+const jwt = require("jsonwebtoken");
 
 async function register(req, res) {
   try {
@@ -54,6 +55,59 @@ async function register(req, res) {
   }
 }
 
+async function login(req, res) {
+
+  try {
+
+    const { email, password } = req.body;
+
+    const result = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({
+        error: "Invalid credentials"
+      });
+    }
+
+    const user = result.rows[0];
+
+    const validPassword = await bcrypt.compare(
+      password,
+      user.password_hash
+    );
+
+    if (!validPassword) {
+      return res.status(401).json({
+        error: "Invalid credentials"
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d"
+      }
+    );
+
+    res.json({
+      token
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+}
+
 module.exports = {
-  register
+  register, login
 };
+
