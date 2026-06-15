dd9e83215abd2ec71fee272bfaa5858aef30308a
Varun Dhyani <varundhyani29@gmail.com>
2026-06-15 05:50:45 +0530

    added base url in env

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use(config => {
@@ -17,6 +17,7 @@ api.interceptors.request.use(config => {
   }
 
   return config;
+
 });
 
 export default api;
